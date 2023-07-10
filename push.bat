bb5aad1047006e9015faa8384a8a9b95206ffab9
Baymax1470 <baymax1470@163.com>
2023-07-10 18:25:53 +0800

    鈥渦pload鈥?

---
+++ push.bat
@@ -0,0 +1,9 @@
+git init
+
+git add .
+
+git commit -m “upload”
+
+git remote add origin https://github.com/baymax1470/pixelgame.git   
+                                   
+git push -u origin main 
